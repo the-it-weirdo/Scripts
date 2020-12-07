7697600654fce992fcef3ab9384c3466ac028b11
the-it-weirdo <ddsmegh4@gmail.com>
2020-12-07 13:56:56 +0530

    Modifies javr to permit different directory compile and run. Doesn't support different drive run yet

--- javr/javr.bat
+++ javr/javr.bat
@@ -1,8 +1,12 @@
 @echo off
-IF [%1] == [] GOTO NOPARAM
-javac %1.java
-java %1
-GOTO END
+setlocal
+IF [%1] == [] (CALL :NOPARAM) ELSE (CALL :RUN_JAVA %1)
+GOTO :EOF
+
+:RUN_JAVA
+javac %~dpnx1 && cd %~dp1 && java %~n1
+exit /b
+
 :NOPARAM
-ECHO "No parameter Error"
-:END
+ECHO No parameter Error
+exit /b
